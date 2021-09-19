d756f37cd1d8aaf54e06ef87292b948cff4b285f
Samuel Medina <smdoomed@gmail.com>
2021-09-18 19:27:41 -0500

    agregar mensajes

--- src/main/java/MensajesService.java
+++ src/main/java/MensajesService.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class MensajesService {
     public static void crearMensaje(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresa tu mensaje :");
+        String mensaje = sc.nextLine();
+
+        System.out.println("Ingresa tu nombre :");
+        String nombre = sc.nextLine();
+
+        Mensajes nuevoMensaje = new Mensajes();
+        nuevoMensaje.setMensaje(mensaje);
+        nuevoMensaje.setAutor(nombre);
+
+        MensajesDAO.createMensajeDB(nuevoMensaje);
 
     }
 
